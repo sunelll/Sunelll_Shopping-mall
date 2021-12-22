2e20458d4afa36c01db48b6428e158661ca39f21
sunelll <43489093+sunelll@users.noreply.github.com>
2021-12-23 00:24:20 +0900

    hesu
    1222 MongoDB-mongoose 연결

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const res = require('express/lib/response')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')// Using Node.js `require()`
+//import mongoose from 'mongoose'// Using ES6 imports
+
+
+//nelll, abcd1234
+mongoose.connect('mongodb+srv://nell:abcd1234@boilerplate.3qc40.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err=> console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
