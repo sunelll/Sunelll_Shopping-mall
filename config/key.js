2b710134650351dcc85d2cc9afe25425be8828fb
sunelll <43489093+sunelll@users.noreply.github.com>
2021-12-30 17:57:02 +0900

    hesu
    회원 가입 기능,
    nodemon backend,
    dev.js를 통한 MONGODB_key 숨김

---
+++ config/key.js
@@ -0,0 +1,5 @@
+if(process.env.NODE_ENV=='prodution'){
+    module.exports = require('./prod')
+} else {
+    module.exports = require('./dev')
+}
--- index.js
+++ index.js
@@ -5,16 +5,39 @@ const port = 5000
 
 const mongoose = require('mongoose')// Using Node.js `require()`
 //import mongoose from 'mongoose'// Using ES6 imports
+const bodyParser = require('body-parser')
 
+const config = require('./config/key')
+const { User } = require("./model/User");
 
-//nelll, abcd1234
-mongoose.connect('mongodb+srv://nell:abcd1234@boilerplate.3qc40.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  
+//application/x-www-form-urlncoded <이런 형태의 코드를 분석해서 가지고 올수 있게 해주는 것
+app.use(bodyParser.urlencoded({extended: true}))
+//application/json을 분석해서 가지고 올 수 있게 해주는 것.
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI , {  
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err=> console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 오늘은 1월입니다! 새해복 만이 받으세용!')
+})
+
+app.post('/register', (req, res) => { 
+  //회원 가입 할 때 필요한 정보드를 CLIENT에서 가져오면,
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  //몽고DB에 저장, 만약 에러가 있다면 클라이언트에 에러가 있다고 json 형식으로 전달하고, 에러 메세지도 전달.
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+
 })
 
 app.listen(port, () => {
