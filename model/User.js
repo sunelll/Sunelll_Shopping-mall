77b2a65204c79c9f4fd0cbd4934f5152e96b580e
sunelll <43489093+sunelll@users.noreply.github.com>
2021-12-31 13:24:44 +0900

    hesu
    bcrypt을 이용한 비밀번호 암호화

--- model/User.js
+++ model/User.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const res = require('express/lib/response');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +35,24 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
+})
+
+userSchema.pre('save', function( next ){
+    var user = this;
+
+    if(user.isModified('password')){
+        // 비밀번호를 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+
 })
 
 const User = mongoose.model('User', userSchema)
