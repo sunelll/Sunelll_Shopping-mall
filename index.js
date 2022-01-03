bcdba01ab53e097d587661bb04065bec3687cead
sunelll <43489093+sunelll@users.noreply.github.com>
2022-01-03 23:56:43 +0900

    hesu
    22-01-03 logout 기능 구현

--- index.js
+++ index.js
@@ -74,7 +74,30 @@ app.post('/api/users/login', (req, res) => {
 
 //Auth 기능
 app.get('/api/users/auth', auth, (req, res) =>{
+  
+  //여기까지 미들웨어를통과해 왔다는 얘기는 AUthentication 이 True 라는 말
+  res.status(200).json({
+    _id:req.user_id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
 
+app.get('/api/users/logout', auth, (req, res) =>
+{
+  User.findOneAndUpdate({_id: req.user._id} ,
+    {token: ""}
+    , (err, user) =>{
+      if(err) returnres.json({ success: false, err })
+      return res.status(200).send({
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => {
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,5 @@
+const { User } = require('../model/User');
+
 let auth = (req, res, next) => {
 
     //인증 처리를 하는 곳.
@@ -6,10 +8,15 @@ let auth = (req, res, next) => {
     let token = req.cookies.x_auth
     
     //2. 토큰을 복호화한다음에 같은 키를 가진 유저를 찾는다.
-    
-    //3. 유저가 있으면 인증 OK / 유저가 없으면 인증 NO
+    User.findByToken(token, (err, user) => {
+        if(err) throw err;
+        //3. 유저가 있으면 인증 OK / 유저가 없으면 인증 NO
+        if(!user) return res.json({ isAuth: false, error: true })
 
-
+        req.token = token;
+        req.user = user;
+        next();
+    }) 
 }
 
 module.exports = { auth };
